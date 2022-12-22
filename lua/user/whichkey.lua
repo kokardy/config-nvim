b59e60123ff73189d49ede7c35153d42c9de5953
kokardy <2720900+kokardy@users.noreply.github.com>
2022-12-22 23:11:20 +0900

    fix deprecated function & unnecessary comment plugin

--- lua/user/whichkey.lua
+++ lua/user/whichkey.lua
@@ -95,7 +95,7 @@ local mappings = {
   },
   ["F"] = { "<cmd>Telescope live_grep theme=ivy<cr>", "Find Text" },
   ["P"] = { "<cmd>lua require('telescope').extensions.projects.projects()<cr>", "Projects" },
-  ["/"] = { "<cmd>TComment<CR>", "Toggle Comment" },
+  ["/"] = { "<Plug>(comment_toggle_linewise_current)<CR>", "Toggle Comment" },
 
   p = {
     name = "Packer",
