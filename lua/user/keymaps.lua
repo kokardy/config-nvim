3f8103d919cc8f2203aaed0dd9ccf7cdf235d227
kokardy <mgc1028@gmail.com>
2022-12-26 19:54:10 +0900

    config toggle comment

--- lua/user/keymaps.lua
+++ lua/user/keymaps.lua
@@ -60,6 +60,12 @@ keymap("x", "K", ":move '<-2<CR>gv-gv", opts)
 keymap("x", "<A-j>", ":move '>+1<CR>gv-gv", opts)
 keymap("x", "<A-k>", ":move '<-2<CR>gv-gv", opts)
 
+
+-- comment
+
+keymap("n", "<Space>/", "gcc", {} )
+keymap("v", "<Space>/", "gcc<Esc>", {} )
+
 -- Terminal --
 
 keymap("n", "<C-t>", ":ToggleTerm<CR>", opts)
