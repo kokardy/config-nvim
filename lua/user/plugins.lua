960c35ba50e817e1498b1deb3750646a8e11ab33
kokardy <2720900+kokardy@users.noreply.github.com>
2022-12-26 13:57:34 +0900

    add plugin for mark

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -205,7 +205,7 @@ return packer.startup(function(use)
   use({ "t9md/vim-quickhl" })
 
   -- nerdfont
-  use({"lambdalisue/nerdfont.vim"})
+  use({ "lambdalisue/nerdfont.vim" })
 
   -- prevent from saving typo file
   use({ "tyru/stoptypofile.vim" })
@@ -304,10 +304,46 @@ return packer.startup(function(use)
       --[[ "julia", ]]
       --[[ "haskell", ]]
     },
-    config = function() 
+    config = function()
       require("user.dap.init").setup()
     end
   })
+
+  -- mark
+  use({ "chentoast/marks.nvim",
+    config = function()
+      require 'marks'.setup {
+        -- whether to map keybinds or not. default true
+        default_mappings = true,
+        -- which builtin marks to show. default {}
+        builtin_marks = { ".", "<", ">", "^" },
+        -- whether movements cycle back to the beginning/end of buffer. default true
+        cyclic = true,
+        -- whether the shada file is updated after modifying uppercase marks. default false
+        force_write_shada = false,
+        -- how often (in ms) to redraw signs/recompute mark positions.
+        -- higher values will have better performance but may cause visual lag,
+        -- while lower values may cause performance penalties. default 150.
+        refresh_interval = 250,
+        -- sign priorities for each type of mark - builtin marks, uppercase marks, lowercase
+        -- marks, and bookmarks.
+        -- can be either a table with all/none of the keys, or a single number, in which case
+        -- the priority applies to all marks.
+        -- default 10.
+        sign_priority = { lower = 10, upper = 15, builtin = 8, bookmark = 20 },
+        -- disables mark tracking for specific filetypes. default {}
+        excluded_filetypes = {},
+        -- marks.nvim allows you to configure up to 10 bookmark groups, each with its own
+        -- sign/virttext. Bookmarks can be used to group together positions and quickly move
+        -- across multiple buffers. default sign is '!@#$%^&*()' (from 0 to 9), and
+        -- default virt_text is "".
+        bookmark_0 = {
+          sign = "⚑",
+          virt_text = "hello world"
+        },
+        mappings = {}
+      }
+    end })
 
   -- Automatically set up your configuration after cloning packer.nvim
   -- Put this at the end after all plugins
