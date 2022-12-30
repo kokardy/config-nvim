320bd272887c335b145ad025f5ed1a71308de6ed
kokardy <2720900+kokardy@users.noreply.github.com>
2022-12-30 11:25:30 +0900

    install script

--- install.sh
+++ install.sh
@@ -1,3 +1,20 @@
+#!/bin/bash
+set -x
+
+cd ~/.config/nvim
+
+# dependencies
 sudo apt-get install luarocks
 sudo npm install --global yarn tree-sitter prettier pyright
 cargo install tree-sitter-cli remark stylua ripgrep fd-find
+
+# font install
+HAKUGEN_VER = "v2.8.0"
+wget "https://github.com/yuru7/HackGen/releases/download/${HAKUGEN_VER}/HackGen_${HAKUGEN_VER}.zip"
+unzip -D HackGen_${HAKUGEN_VER}.zip
+mkdir -p ~/.fonts
+cp HackGen_${HAKUGEN_VER}/*.ttf ~/.fonts/
+fc-cache -v
+
+rm -f ./HackGen_*.zip
+rm -rf ./HackGen_*
