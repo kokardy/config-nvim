31d866c7e36a5c2b91acf001b5a9318f4b2a104f
kokardy <mgc1028@gmail.com>
2023-01-03 11:40:10 +0900

    add plugin for asterisk

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -378,6 +378,17 @@ return packer.startup(function(use)
   -- buffer
   use({ "kwkarlwang/bufresize.nvim" })
 
+  -- asterisk
+  use({ "rapan931/lasterisk.nvim",
+    config = function()
+      vim.keymap.set('n', '*', function() require("lasterisk").search() end)
+      vim.keymap.set('n', 'g*', function() require("lasterisk").search({ is_whole = false }) end)
+      vim.keymap.set('x', 'g*', function() require("lasterisk").search({ is_whole = false }) end)
+
+      -- not support visual asterisk & is_whole = true
+      -- vim.keymap.set('x', '*',  function() require("lasterisk").search() end)
+    end
+  })
 
 
   -- Automatically set up your configuration after cloning packer.nvim
