735f264afd0c9eeb61a530a844bd46d56102a3dc
kokardy <2720900+kokardy@users.noreply.github.com>
2023-01-03 16:22:43 +0900

    cursor highlight

--- lua/user/colorscheme.lua
+++ lua/user/colorscheme.lua
@@ -1,8 +1,12 @@
 vim.cmd [[
 try
   colorscheme industry
+
 catch /^Vim\%((\a\+)\)\=:E185/
   colorscheme default
   set background=dark
 endtry
 ]]
+
+vim.api.nvim_set_hl(0, "CursorLine", {bg="#222222"})
+vim.api.nvim_set_hl(0, "CursorColumn", {bg="#222222"})
