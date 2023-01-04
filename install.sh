44313a88add81dca45c142ec0478d5da20711505
kokardy <2720900+kokardy@users.noreply.github.com>
2023-01-04 17:01:51 +0900

    fix install script: font

--- install.sh
+++ install.sh
@@ -9,11 +9,11 @@ sudo npm install --global yarn tree-sitter prettier pyright
 cargo install tree-sitter-cli remark stylua ripgrep fd-find
 
 # font install
-HAKUGEN_VER = "v2.8.0"
-wget "https://github.com/yuru7/HackGen/releases/download/${HAKUGEN_VER}/HackGen_${HAKUGEN_VER}.zip"
-unzip -D HackGen_${HAKUGEN_VER}.zip
+HAKUGEN_VER="v2.8.0"
+wget "https://github.com/yuru7/HackGen/releases/download/${HAKUGEN_VER}/HackGen_NF_${HAKUGEN_VER}.zip"
+unzip -D HackGen_NF_${HAKUGEN_VER}.zip
 mkdir -p ~/.fonts
-cp HackGen_${HAKUGEN_VER}/*.ttf ~/.fonts/
+cp HackGen_NF_${HAKUGEN_VER}/*.ttf ~/.fonts/
 fc-cache -v
 
 rm -f ./HackGen_*.zip
