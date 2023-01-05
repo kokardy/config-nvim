d766025f42228eabd237e752761f0c98a8f43408
kokardy <2720900+kokardy@users.noreply.github.com>
2023-01-05 14:01:19 +0900

    add plugin for outline

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -207,6 +207,8 @@ return packer.startup(function(use)
       require("todo-comments").setup()
     end,
   })
+
+  -- outline
   use({ "fcying/telescope-ctags-outline.nvim",
     config = function()
       require('telescope').setup {
@@ -229,6 +231,23 @@ return packer.startup(function(use)
       require('telescope').load_extension('ctags_outline')
     end,
   })
+
+  use({ "stevearc/aerial.nvim",
+    config = function()
+      require('aerial').setup({
+        -- optionally use on_attach to set keymaps when aerial has attached to a buffer
+        on_attach = function(bufnr)
+          -- Jump forwards/backwards with '{' and '}'
+          vim.keymap.set('n', '{', '<cmd>AerialPrev<CR>', { buffer = bufnr })
+          vim.keymap.set('n', '}', '<cmd>AerialNext<CR>', { buffer = bufnr })
+        end
+      })
+
+      -- You probably also want to set a keymap to toggle aerial
+      vim.keymap.set('n', '<leader>o', '<cmd>AerialToggle!<CR>')
+    end
+  })
+
 
   -- document
   use({ "danymat/neogen",
