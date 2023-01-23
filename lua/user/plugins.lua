33990212c10f671c0ab8ac7013696ece937f169b
kokardy <2720900+kokardy@users.noreply.github.com>
2023-01-23 18:48:01 +0900

    config plugins   - yanky     resolve keymap conflict [C-k]   - neogen     bad requires plugin

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -302,25 +302,41 @@ return packer.startup(function(use)
         }
       })
     end,
-    requires = {
-      { "nvim-telescope/telescope.nvim", opt = true, },
-    },
+    requires = "nvim-treesitter/nvim-treesitter",
   })
 
   -- yank
   use({
     "gbprod/yanky.nvim",
-    -- requires = "nvim-treesitter/nvim-treesitter",
     requires = {
       { "nvim-telescope/telescope.nvim", opt = true, },
     },
     config = function()
+      local utils = require("yanky.utils")
+      local mapping = require("yanky.telescope.mapping")
       require("yanky").setup({
-        -- highlight = {
-        --   on_put = true
-        --   on_yank = true
-        --   timer = 500
-        -- }
+        picker = {
+          telescope = {
+            mappings = {
+              default = mapping.put("p"),
+              i = {
+                ["C-p"] = mapping.put("p"),
+                -- ["S-P"] = mapping.put("P"),
+                -- [your keymap] = mapping.delete(),
+                -- [your keymap] = mapping.set_register(utils.get_default_register()),
+              },
+              -- n = {
+              --   [your keymap] = mapping.put("p"),
+              --   [your keymap] = mapping.put("P"),
+              --   [your keymap] = mapping.delete(),
+              --   [your keymap] = mapping.set_register(utils.get_default_register())
+              -- },
+            }
+          },
+          -- select = {
+          --   action = nil, -- nil to use default put action
+          -- },
+        },
       })
       require("telescope").load_extension("yank_history")
       vim.keymap.set('n', '<leader>yr', '<cmd>Telescope yank_history<CR>')
