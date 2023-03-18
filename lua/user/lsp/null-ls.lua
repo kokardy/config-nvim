c9bf59754ab1f2dba66c7c2a7fdd76353b136199
kokardy <2720900+kokardy@users.noreply.github.com>
2023-03-18 16:06:42 +0900

    Change null-ls ruff setting: disable fix

--- lua/user/lsp/null-ls.lua
+++ lua/user/lsp/null-ls.lua
@@ -8,56 +8,31 @@ local formatting = null_ls.builtins.formatting
 -- https://github.com/jose-elias-alvarez/null-ls.nvim/tree/main/lua/null-ls/builtins/diagnostics
 local diagnostics = null_ls.builtins.diagnostics
 
-local methods = require("null-ls.methods")
-local helpers = require("null-ls.helpers")
-local function ruff_fix()
-  return helpers.make_builtin({
-    name = "ruff",
-    meta = {
-      url = "https://github.com/charliermarsh/ruff/",
-      description = "An extremely fast Python linter, written in Rust.",
-    },
-    method = methods.internal.FORMATTING,
-    filetypes = { "python" },
-    generator_opts = {
-      command = "ruff",
-      args = { "--fix", "-e", "-n", "--stdin-filename", "$FILENAME", "-" },
-      to_stdin = true
-    },
-    factory = helpers.formatter_factory
-  })
-end
-
 null_ls.setup({
   debug = false,
   sources = {
-    --ruff fix
-    ruff_fix(),
-    null_ls.builtins.diagnostics.ruff,
 
-    -- format
-    formatting.prettier.with({
-      filetypes = { "javascript", "typescript", "json" },
-      extra_args = { "--no-semi", "--single-quote", "--jsx-single-quote" },
+    -- python
+    diagnostics.ruff.with({ 
+      filetypes = {"python"},
     }),
-    formatting.isort.with({ filetypes = { "python" } }),
-    formatting.black.with({ filetypes = { "python" }, extra_args = { "--fast" } }),
-    formatting.remark.with({ filetypes = { "markdown" } }),
-    -- formatting.stylua.with({
-    --   filetypes = { "lua" },
-    --   extra_args = {
-    --     "--indent-type=Spaces",
-    --     "--indent-width=2",
-    --   },
-    -- }),
-
-    -- diagnostics
-    -- diagnostics.flake8
-    -- diagnostics.pyproject_flake8.with({ filetype = { "python" } }),
     diagnostics.mypy.with({
       filetype = { "python" },
       extra_args = { "--strict" },
     }),
+    formatting.isort.with({ filetypes = { "python" } }),
+    formatting.black.with({ filetypes = { "python" }, extra_args = { "--fast" } }),
+
+    -- javascript
+    formatting.prettier.with({
+      filetypes = { "javascript", "typescript", "json" },
+      extra_args = { "--no-semi", "--single-quote", "--jsx-single-quote" },
+    }),
+
+    -- markdown
+    formatting.remark.with({ filetypes = { "markdown" } }),
+
+    -- json
     diagnostics.jsonlint.with({ filetype = { "json" } }),
 
     -- typo
@@ -79,7 +54,8 @@ null_ls.setup({
         "go.mod",
         ".git",
       }
-      return util.root_pattern(unpack(root_files))(fname) or util.root_pattern ".git" (fname) or util.path.dirname(fname)
+      local root = util.root_pattern(unpack(root_files))(fname) or util.root_pattern ".git" (fname) or util.path.dirname(fname)
+      return root
     end,
   },
 })
