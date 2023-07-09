b6798a50a573b17c27cae3c6b91fbfb0d0269cc8
kokardy <2720900+kokardy@users.noreply.github.com>
2023-07-09 16:32:51 +0900

    Enable to format markdown table in visual mode [<space>t]

--- lua/user/autocommands.lua
+++ lua/user/autocommands.lua
@@ -17,6 +17,7 @@ vim.cmd [[
     autocmd!
     autocmd FileType markdown setlocal wrap
     autocmd FileType markdown setlocal spell
+    autocmd FileType markdown vmap <Leader>t :EasyAlign*<Bar><Enter>
   augroup end
 
   augroup _auto_resize
