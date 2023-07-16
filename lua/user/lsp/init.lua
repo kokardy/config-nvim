6c5433afe50b2a491bfb02183d0c7fb15fc4a140
kokardy <2720900+kokardy@users.noreply.github.com>
2023-07-16 21:20:11 +0900

    Fix issue LSP doesn't work

--- lua/user/lsp/init.lua
+++ lua/user/lsp/init.lua
@@ -3,8 +3,8 @@ if not status_ok then
 	return
 end
 
-require("user.lsp.configs")
+require("user.lsp.configs").setup()
 require("user.lsp.handlers").setup()
-require("user.lsp.null-ls")
+-- require("user.lsp.null-ls")
 
 require("user.lsp.efm-langserver").setup()
