dfccf1dd5883efe267f73ee744c2b547612190fc
kokardy <2720900+kokardy@users.noreply.github.com>
2023-07-19 00:23:11 +0900

    Autocmd highlight on changing colorscheme

--- lua/user/colorscheme.lua
+++ lua/user/colorscheme.lua
@@ -1,4 +1,24 @@
-vim.cmd [[
+
+vim.api.nvim_set_hl(0, "CursorLine", { bg = "#222222" })
+vim.api.nvim_set_hl(0, "CursorColumn", { bg = "#222222" })
+
+vim.api.nvim_create_augroup("TransparentBG", {})
+vim.api.nvim_create_autocmd("ColorScheme", {
+	group = "TransparentBG",
+	callback = function()
+		print("colorscheme change")
+		vim.api.nvim_set_hl(0, "Normal", { cterm = nil })
+		vim.api.nvim_set_hl(0, "NonText", { cterm = nil })
+		vim.api.nvim_set_hl(0, "LineNr", { cterm = nil })
+		vim.api.nvim_set_hl(0, "Folded", { cterm = nil })
+		vim.api.nvim_set_hl(0, "EndOfBuffer", { cterm = nil })
+		vim.api.nvim_set_hl(0, "NvimTreeNormal", { cterm = nil })
+		vim.api.nvim_set_hl(0, "NvimTreeBg", { cterm = nil })
+		vim.api.nvim_set_option("winhighlight", "Normal:NvimTreeBg")
+	end,
+})
+
+vim.cmd([[
 try
   colorscheme darkplus
 
@@ -6,28 +26,4 @@ catch /^Vim\%((\a\+)\)\=:E185/
   colorscheme default
   set background=dark
 endtry
-]]
-
-vim.api.nvim_set_hl(0, "CursorLine", { bg = "#222222" })
-vim.api.nvim_set_hl(0, "CursorColumn", { bg = "#222222" })
-
--- background
-vim.api.nvim_set_hl(0, "Normal", { ctermbg = nil })
-vim.api.nvim_set_hl(0, "NonText", { ctermbg = nil })
-vim.api.nvim_set_hl(0, "LineNr", { ctermbg = nil })
-vim.api.nvim_set_hl(0, "Folded", { ctermbg = nil })
-vim.api.nvim_set_hl(0, "EndOfBuffer", { ctermbg = nil })
-
--- TODO: not work
-vim.cmd[[
-  augroup TransparentBG
-    autocmd!
-    autocmd Colorscheme * highlight Normal ctermbg=none
-    autocmd Colorscheme * highlight NonText ctermbg=none
-    autocmd Colorscheme * highlight LineNr ctermbg=none
-    autocmd Colorscheme * highlight Folded ctermbg=none
-    autocmd Colorscheme * highlight EndOfBuffer ctermbg=none 
-  augroup END
-]]
-
-
+]])
