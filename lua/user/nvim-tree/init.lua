c48a909cc30e4dea9f3ddc2c65ef299e6e8377ef
kokardy <2720900+kokardy@users.noreply.github.com>
2023-08-22 22:41:03 +0900

    Fix: nvim-tree does not work

--- lua/user/nvim-tree/init.lua
+++ lua/user/nvim-tree/init.lua
@@ -3,18 +3,11 @@ if not status_ok then
   return
 end
 
-local config_status_ok, nvim_tree_config = pcall(require, "nvim-tree.config")
-if not config_status_ok then
-  return
-end
-
-local tree_cb = nvim_tree_config.nvim_tree_callback
-
-local on_attach = require("user.nvim-tree.nvim-tree-on-attach").on_attach
+-- local on_attach = require("user.nvim-tree.nvim-tree-on-attach").on_attach
 
 nvim_tree.setup {
   -- mapping key
-  on_attach=on_attach,
+  -- on_attach=on_attach,
 
   update_focused_file = {
     enable = true,
@@ -61,12 +54,5 @@ nvim_tree.setup {
   view = {
     width = 30,
     side = "left",
-    -- mappings = {
-    --   list = {
-    --     { key = { "l", "<CR>", "o" }, cb = tree_cb "edit" },
-    --     { key = "h", cb = tree_cb "close_node" },
-    --     { key = "v", cb = tree_cb "vsplit" },
-    --   },
-    -- },
   },
 }
