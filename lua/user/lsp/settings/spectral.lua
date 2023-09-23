b7a06fc7b18e8dbbca98f66018c7f896f3ada63e
kokardy <2720900+kokardy@users.noreply.github.com>
2023-09-23 21:03:58 +0900

    Config LSP settigns: spectral lint removed spectral-language-server and enabled efm-langserver spectral lint

--- lua/user/lsp/settings/spectral.lua
+++ lua/user/lsp/settings/spectral.lua
@@ -1,4 +1,17 @@
+local root_dir = function(fname)
+	local util = require("lspconfig.util")
+	local root_files = {
+		".spectral.yaml",
+		".git",
+	}
+	local root = util.root_pattern(unpack(root_files))(fname)
+		or util.root_pattern(".git")(fname)
+		or util.path.dirname(fname)
+	return root
+end
+
 return {
 	cmd = { "spectral-language-server", "-F hint", "--stdio" },
 	filetypes = { "yaml", "yaml.openapi", "json.openapi" },
+	root_dir = root_dir,
 }
--- testdata/mypy_error.py
+++ testdata/mypy_error.py
@@ -1,5 +1,7 @@
 def main() -> None:
     print("OK" )
+    a = ""
+    a = 3
 
 
 if __name__ == "__main__":
