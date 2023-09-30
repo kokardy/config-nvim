f9fa59e37508abd79c7715d21e45996fce06578e
kokardy <mgc1028@gmail.com>
2023-09-30 18:53:14 +0900

    Config treesitter_ensure_installed list

--- lua/user/treesitter.lua
+++ lua/user/treesitter.lua
@@ -3,19 +3,23 @@ if not status_ok then
 	return
 end
 
+-- local parser_dir = "~/.config/nvim/tsparsers"
+-- vim.opt.runtimepath:append(parser_dir)
+
 configs.setup({
-	-- parser_install_dir = "~/.config/nvim/tsparsers",
+	-- parser_install_dir = parser_dir,
 
 	--[[ ensure_installed = "all", -- one of "all" or a list of languages ]]
 	ensure_installed = {
-		-- "python",
-		-- "svelte",
-		-- "javascript",
-		-- "typescript",
+		"lua",
+		"python",
+		"svelte",
+		"javascript",
+		"typescript",
+		"markdown",
 	},
 	sync_install = false,
 	auto_install = false,
-
 	ignore_install = { "phpdoc" }, -- List of parsers to ignore installing
 	highlight = {
 		enable = true, -- false will disable the whole extension
@@ -25,10 +29,11 @@ configs.setup({
 		enable = true,
 	},
 	indent = { enable = true, disable = { "python", "css" } },
-
 	-- rainbow
 	rainbow = {
 		enable = true,
 		extended_mode = true,
 	},
 })
+
+vim.treesitter.language.register("yaml", { "yaml", "openapi" })
