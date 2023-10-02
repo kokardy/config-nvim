9b23ec88cce783ce0a62f99dd0ad1950d7dcb2a8
kokardy <mgc1028@gmail.com>
2023-10-02 11:30:57 +0900

    Config efm-langserver for spectral

--- lua/user/lsp/settings/efm.lua
+++ lua/user/lsp/settings/efm.lua
@@ -1,58 +1,61 @@
 return {
-  init_options = { documentFormatting = true },
-  settings = {
-    rootMarkers = {
-      ".spectral.yaml",
-      "pyproject.toml",
-      "package.json",
-      "setup.py",
-      "setup.cfg",
-      "requirements.txt",
-      "Pipfile",
-      "manage.py",
-      "pyrightconfig.json",
-      "go.mod",
-      ".git",
-    },
+	init_options = { documentFormatting = true },
+	settings = {
+		rootMarkers = {
+			"pyproject.toml",
+			"package.json",
+			"setup.py",
+			"setup.cfg",
+			"requirements.txt",
+			"Pipfile",
+			"manage.py",
+			"pyrightconfig.json",
+			"go.mod",
+			".git",
+		},
 
-    languages = {
-      -- lua
-      lua = {
-        { formatCommand = "stylua -", formatStdin = true },
-      },
-      -- python
-      python = {
-        { formatCommand = "isort -",                          formatStdin = true },
-        { formatCommand = "black -",                          formatStdin = true },
-        { lintCommand = "ruff -" },
-        { lintCommand = "mypy --strict --show-column-numbers" },
-      },
-      -- json
-      -- json = {
-      -- 	{ formatCommand = "prettier"},
-      -- },
+		languages = {
+			-- lua
+			lua = {
+				{ formatCommand = "stylua -", formatStdin = true },
+			},
+			-- python
+			python = {
+				{ formatCommand = "isort -", formatStdin = true },
+				{ formatCommand = "black -", formatStdin = true },
+				{ lintCommand = "ruff -" },
+				{ lintCommand = "mypy --strict --show-column-numbers" },
+			},
+			-- json
+			-- json = {
+			-- 	{ formatCommand = "prettier"},
+			-- },
 
-      -- javascript
-      javascript = {
-        { formatCommand = "prettier --single-quote" },
-      },
+			-- javascript
+			javascript = {
+				{ formatCommand = "prettier --single-quote" },
+			},
 
-      -- jsx
-      javascriptreact = {
-        { formatCommand = "prettier --no-semi --single-quote --jsx-single-quote" },
-      },
+			-- jsx
+			javascriptreact = {
+				{ formatCommand = "prettier --no-semi --single-quote --jsx-single-quote" },
+			},
 
-      -- markdown
-      markdown = {
-        { formatCommand = "remark", formatStdin = true },
-      },
+			-- markdown
+			markdown = {
+				{ formatCommand = "remark", formatStdin = true },
+			},
 
-      -- spectral
-      openapi = {
-        { lintCommand = "spectral lint -f text -F hint", lintStdin = false },
-        -- リダイレクトでテストできる
-        -- { lintCommand = "echo OK >> /tmp/yaml.log",                                       lintStdin = false },
-      },
-    },
-  },
+			-- spectral
+			openapi = {
+				{
+					lintCommand = "spectral lint -f text -F hint",
+					lintStdin = false,
+					rootMarkers = { ".spectral.yaml" },
+				},
+				-- リダイレクトでテストできる
+				-- { lintCommand = "echo OK >> /tmp/yaml.log",                                       lintStdin = false },
+			},
+		},
+	},
 }
