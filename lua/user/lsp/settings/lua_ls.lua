cc705ec0455cf76c935bd9b11b7f530d458fcd25
kokardy <2720900+kokardy@users.noreply.github.com>
2024-11-30 15:17:30 +0900

    Apply stylua

--- lua/user/lsp/settings/lua_ls.lua
+++ lua/user/lsp/settings/lua_ls.lua
@@ -1,20 +1,20 @@
 return {
-  cmd = { "lua-language-server" },
-  settings = {
+	cmd = { "lua-language-server" },
+	settings = {
 
-    Lua = {
-      runtime = { version = "LuaJIT" },
-      diagnostics = {
-        globals = { "vim" },
-      },
-      workspace = {
-        preloadFileSize = 3000,
-        library = {
-          [vim.fn.expand("$VIMRUNTIME/lua")] = true,
-          [vim.fn.stdpath("config") .. "/lua"] = true,
-        },
-      },
-      format = false,
-    },
-  },
+		Lua = {
+			runtime = { version = "LuaJIT" },
+			diagnostics = {
+				globals = { "vim" },
+			},
+			workspace = {
+				preloadFileSize = 3000,
+				library = {
+					[vim.fn.expand("$VIMRUNTIME/lua")] = true,
+					[vim.fn.stdpath("config") .. "/lua"] = true,
+				},
+			},
+			format = false,
+		},
+	},
 }
