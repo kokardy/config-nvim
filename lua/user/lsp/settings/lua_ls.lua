4856e1b24d3e205954ffda3f1619506587f2c4f1
kokardy <mgc1028@gmail.com>
2024-11-30 15:15:31 +0900

    Lua formatter: disable lua_ls , use stylua from efm

--- lua/user/lsp/settings/lua_ls.lua
+++ lua/user/lsp/settings/lua_ls.lua
@@ -1,19 +1,20 @@
 return {
-	cmd = { "lua-language-server" },
-	settings = {
+  cmd = { "lua-language-server" },
+  settings = {
 
-		Lua = {
-			runtime = { version = "LuaJIT" },
-			diagnostics = {
-				globals = { "vim" },
-			},
-			workspace = {
-				preloadFileSize = 3000,
-				library = {
-					[vim.fn.expand("$VIMRUNTIME/lua")] = true,
-					[vim.fn.stdpath("config") .. "/lua"] = true,
-				},
-			},
-		},
-	},
+    Lua = {
+      runtime = { version = "LuaJIT" },
+      diagnostics = {
+        globals = { "vim" },
+      },
+      workspace = {
+        preloadFileSize = 3000,
+        library = {
+          [vim.fn.expand("$VIMRUNTIME/lua")] = true,
+          [vim.fn.stdpath("config") .. "/lua"] = true,
+        },
+      },
+      format = false,
+    },
+  },
 }
