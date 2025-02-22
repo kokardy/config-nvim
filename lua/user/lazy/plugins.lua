62dd7068aac1f5361e98dbb4a58a7d9577cd5653
kokardy <2720900+kokardy@users.noreply.github.com>
2025-02-22 16:46:57 +0900

    Add plugin: fine-cmdline.nvim

--- lua/user/lazy/plugins.lua
+++ lua/user/lazy/plugins.lua
@@ -1,8 +1,58 @@
 return {
   -- plugins
   {
-    "nvim-lua/plenary.nvim",
-  }, -- Useful lua functions used by lots of plugins
+    "VonHeikemen/fine-cmdline.nvim",
+    dependencies = {
+      { "MunifTanjim/nui.nvim" },
+    },
+    config = function()
+      vim.api.nvim_set_keymap('n', ':', '<cmd>FineCmdline<CR>', { noremap = true })
+      local fineline = require('fine-cmdline')
+      local fn = fineline.fn
+
+      fineline.setup({
+        cmdline = {
+          -- Prompt can influence the completion engine.
+          -- Change it to something that works for you
+          -- prompt = ': ',
+
+          -- Let the user handle the keybindings
+          enable_keymaps = false
+        },
+        popup = {
+          position = {
+            row = '10%',
+            col = '50%',
+          },
+          size = {
+            width = '60%',
+          },
+          border = {
+            style = 'rounded',
+          },
+          win_options = {
+            winhighlight = 'Normal:Normal,FloatBorder:FloatBorder',
+            winblend=10,
+          },
+          buf_options = {
+            -- Setup a special file type if you need to
+            filetype = 'FineCmdlinePrompt'
+          }
+        },
+        hooks = {
+          set_keymaps = function(imap, feedkeys)
+            -- Restore default keybindings...
+            -- Except for `<Tab>`, that's what everyone uses to autocomplete
+            imap('<Esc>', fn.close)
+            imap('<C-c>', fn.close)
+
+            imap('<Up>', fn.up_search_history)
+            imap('<Down>', fn.down_search_history)
+          end
+        }
+      })
+    end
+  },
 
   {
     "windwp/nvim-autopairs",
