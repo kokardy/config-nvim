d9636d3784953a30e4fd8bcf50ac59b4735a1a2e
kokardy <mgc1028@gmail.com>
2025-06-17 15:48:51 +0900

    lsp keymap now work

--- lua/user/lsp/configs.lua
+++ lua/user/lsp/configs.lua
@@ -70,11 +70,10 @@ M.setup = function()
   })
 
   for _, server in pairs(M.servers) do
-    -- local opts = {
-    --   on_attach = require("user.lsp.handlers").on_attach,
-    --   capabilities = require("user.lsp.handlers").capabilities,
-    -- }
-    local opts = {}
+    local opts = {
+      on_attach = require("user.lsp.handlers").on_attach,
+      capabilities = require("user.lsp.handlers").capabilities,
+    }
     local has_custom_opts, server_custom_opts = pcall(require, "user.lsp.settings." .. server)
     if has_custom_opts then
       opts = vim.tbl_deep_extend("force", opts, server_custom_opts)
