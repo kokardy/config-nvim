d1ccad6549dbcc59abcaf9456811e9a0a96cf7dd
kokardy <2720900+kokardy@users.noreply.github.com>
2025-12-19 08:44:22 +0900

    Add snippet: python decorator

--- testdata/python_project/mypy_error.py
+++ testdata/python_project/mypy_error.py
@@ -1,4 +1,27 @@
-from typing import List
+from typing import Callable
+from functools import wraps
+from datetime import datetime
+import time
+
+
+def timer[**P, R](func: Callable[P, R]) -> Callable[P, R]:
+    """Timer is a decorator that times a function execution."""
+
+    @wraps(func)
+    def wrapper(*args: P.args, **kwargs: P.kwargs) -> R:
+        start = datetime.now()
+        result = func(*args, **kwargs)
+        end = datetime.now()
+        print(f"Function {func.__name__} took {end - start} to execute.")
+        return result
+
+    return wrapper
+
+
+@timer
+def test_timer() -> None:
+    """Test for timer decorator."""
+    time.sleep(1)
 
 
 def main() -> None:
@@ -14,6 +37,7 @@ def main() -> None:
 
     print(my_streaming_response_2)
 
+    test_timer()
 
 
 if __name__ == "__main__":
