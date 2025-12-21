03a0c716a6eb3e1a3a4de3c11e65e452288a4eab
kokardy <mgc1028@gmail.com>
2025-12-21 23:11:09 +0900

    Lazyのplugins整理
      { import = "user.lazy.plugins.colorschemes" },
      { import = "user.lazy.plugins.project" },
      { import = "user.lazy.plugins.status" },
      { import = "user.lazy.plugins.edit" },
      { import = "user.lazy.plugins.filer" },
      { import = "user.lazy.plugins.terminal" },
      { import = "user.lazy.plugins.keymap" },
      { import = "user.lazy.plugins.cmp" },

---
+++ lua/user/lazy/plugins/edit/indent-blankline.lua
@@ -0,0 +1,34 @@
+-- HACK: work-around for https://github.com/lukas-reineke/indent-blankline.nvim/issues/59
+vim.wo.colorcolumn = "99999"
+
+return {
+  "lukas-reineke/indent-blankline.nvim",
+  main = "ibl",
+  opts = {
+    -- show_current_context = true,
+    scope = {
+      enabled = true,
+    },
+    exclude = {
+      buftypes = { "terminal", "nofile" },
+      filetypes = {
+        "''",
+        "NvimTree",
+        "oil",
+        "TelescopePrompt",
+        "TelescopeResults",
+        "Trouble",
+        "checkhealth",
+        "dashboard",
+        "gitcommit",
+        "help",
+        "help",
+        "lspinfo",
+        "man",
+        "neogitstatus",
+        "packer",
+        "startify",
+      },
+    },
+  }
+}
