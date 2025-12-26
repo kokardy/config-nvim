14707e79d3a9d568d42f6ace1e2ec22cdd55ac9e
kokardy <2720900+kokardy@users.noreply.github.com>
2025-12-26 11:27:28 +0900

    change color

--- lua/user/lazy/plugins/edit/flash.lua
+++ lua/user/lazy/plugins/edit/flash.lua
@@ -3,8 +3,6 @@
 return {
   "folke/flash.nvim",
   event = "VeryLazy",
-  ---@type Flash.Config
-  opts = {},
   -- stylua: ignore
   keys = {
     { "s",     mode = { "n", "x", "o" }, function() require("flash").jump() end,              desc = "Flash" },
@@ -13,4 +11,13 @@ return {
     { "R",     mode = { "o", "x" },      function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
     { "<c-s>", mode = { "c" },           function() require("flash").toggle() end,            desc = "Toggle Flash Search" },
   },
+
+  config = function()
+    local flash = require("flash")
+
+    -- highlight color for flash
+    vim.api.nvim_set_hl(0, "FlashLabel", { fg = "#FF0000", bg= "#00FFFF" })
+
+    flash.setup({})
+  end
 }
