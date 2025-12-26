4d4cd3bab96d21f3cff12782a2073eff36f406bb
kokardy <mgc1028@gmail.com>
2025-12-26 11:36:21 +0900

    change color

--- lua/user/lazy/plugins/colorschemes/cyberdream.lua
+++ lua/user/lazy/plugins/colorschemes/cyberdream.lua
@@ -1,4 +1,18 @@
 -- colorscheme
 return {
   "scottmckendry/cyberdream.nvim",
+
+  config = function()
+    local ok, cyberdream = pcall(require, "cyberdream")
+    if not ok then
+      return
+    end
+    cyberdream.setup({
+      transparent = true,
+      options = {
+        theme = "auto",
+      }
+    })
+
+  end,
 }
