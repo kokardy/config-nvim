3571cfba1c866814204af1385de70b456f54cfa9
kokardy <mgc1028@gmail.com>
2025-12-26 10:47:13 +0900

    change color

--- lua/user/colorscheme.lua
+++ lua/user/colorscheme.lua
@@ -1,21 +1,18 @@
 -- colorscheme
 
-vim.api.nvim_set_hl(0, "CursorLine", { bg = "#222222" })
-vim.api.nvim_set_hl(0, "CursorColumn", { bg = "#222222" })
+-- vim.api.nvim_set_hl(0, "CursorLine", { bg = "#222222" })
+-- vim.api.nvim_set_hl(0, "CursorColumn", { bg = "#222222" })
 
 vim.api.nvim_create_augroup("TransparentBG", {})
 vim.api.nvim_create_autocmd("ColorScheme", {
 	group = "TransparentBG",
 	callback = function()
-		vim.api.nvim_set_hl(0, "Normal", { cterm = nil })
-		vim.api.nvim_set_hl(0, "NonText", { cterm = nil })
-		vim.api.nvim_set_hl(0, "LineNr", { cterm = nil })
-		vim.api.nvim_set_hl(0, "Folded", { cterm = nil })
+		-- vim.api.nvim_set_hl(0, "Normal", { cterm = nil })
+		-- vim.api.nvim_set_hl(0, "NonText", { cterm = nil })
+		-- vim.api.nvim_set_hl(0, "LineNr", { cterm = nil })
+		-- vim.api.nvim_set_hl(0, "Folded", { cterm = nil })
 		vim.api.nvim_set_hl(0, "EndOfBuffer", { cterm = nil })
-		vim.api.nvim_set_hl(0, "NvimTreeNormal", { cterm = nil })
-		vim.api.nvim_set_hl(0, "NvimTreeBg", { cterm = nil })
 		vim.api.nvim_set_hl(0, "Comment", { fg = "#FF00FF" })
-		vim.api.nvim_set_option("winhighlight", "Normal:NvimTreeBg")
 	end,
 })
 
